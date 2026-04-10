4cedc289ae46871f138bbc7fb048cfe4a9fd0322
ilyamiro <ilyamiro.work@gmail.com>
2026-04-10 18:41:11 +0200

    fix the degrees being misconfigured due to locale issues

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # ==============================================================================
 # Script Versioning & Initialization
 # ==============================================================================
-DOTS_VERSION="1.0.22"
+DOTS_VERSION="1.0.23"
 VERSION_FILE="$HOME/.local/state/imperative-dots-version"
 
 # Global Variables & Initial States (Defaults)
