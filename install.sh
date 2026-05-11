2658de91121fd1ec05a66cd1ab3d4f2d31738c5f
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2026-05-11 18:11:22 +0630

    added 'xdg-desktop-portal-wlr' for screen/display capturing support

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ ARCH_PKGS=(
     "grim" "playerctl" "satty" "yq" "xdg-desktop-portal-gtk" "slurp" "mpvpaper"
     "wmctrl" "power-profiles-daemon" "easyeffects" "swayosd-git" "nautilus" "lsp-plugins" "hyprpolkitagent"
     "qt5-wayland" "qt5-quickcontrols" "qt5-quickcontrols2" "qt5-graphicaleffects" "qt6-wayland"
-    "qt5ct" "qt6ct" "gpu-screen-recorder" "adw-gtk-theme"
+    "qt5ct" "qt6ct" "gpu-screen-recorder" "adw-gtk-theme" "xdg-desktop-portal-wlr"
 )
 
 PKGS=("${ARCH_PKGS[@]}")
